3971a1d3efb654f924bb7f40d398e9993f8defb8
Vinny Martinez <vinny@vinnymartinez.com>
2017-09-26 00:50:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     num_3 = Kernel.rand(1..20)
 
     if num_1 && num_2 && num_3
-      resp.write "You Win"
+      resp.write num_1
     else
       resp.write "You Lose"
     end
